4022474a814906a25a26d33d40f6e0af4adeedd4
Lily Anderson <thatoanderson92@gmail.com>
2025-07-19 11:42:06 -0400

    Create jsx for cards

---
+++ src/components/ParkCards/ParkCards.jsx
@@ -0,0 +1,89 @@
+import "./ParkCards.css";
+
+function Cards() {
+  return (
+    <div className="cards">
+      <ul className="cards__container">
+        <li className="card">
+          <img src="" alt="I'm a park image" className="card__image" />
+          <div className="card__heading">
+            <h3 className="card__title">Title</h3>
+            <p className="card__date">date</p>
+          </div>
+          <p className="card__location">location</p>
+          <div className="card__info">
+            <p className="card__info-paragraph">information about park</p>
+            <p className="card__info-highlights">highlights</p>
+          </div>
+        </li>
+
+        <li className="card">
+          <img src="" alt="I'm a park image" className="card__image" />
+          <div className="card__heading">
+            <h3 className="card__title">Title</h3>
+            <p className="card__date">date</p>
+          </div>
+          <p className="card__location">location</p>
+          <div className="card__info">
+            <p className="card__info-paragraph">information about park</p>
+            <p className="card__info-highlights">highlights</p>
+          </div>
+        </li>
+
+        <li className="card">
+          <img src="" alt="I'm a park image" className="card__image" />
+          <div className="card__heading">
+            <h3 className="card__title">Title</h3>
+            <p className="card__date">date</p>
+          </div>
+          <p className="card__location">location</p>
+          <div className="card__info">
+            <p className="card__info-paragraph">information about park</p>
+            <p className="card__info-highlights">highlights</p>
+          </div>
+        </li>
+
+        <li className="card">
+          <img src="" alt="I'm a park image" className="card__image" />
+          <div className="card__heading">
+            <h3 className="card__title">Title</h3>
+            <p className="card__date">date</p>
+          </div>
+          <p className="card__location">location</p>
+          <div className="card__info">
+            <p className="card__info-paragraph">information about park</p>
+            <p className="card__info-highlights">highlights</p>
+          </div>
+        </li>
+
+        <li className="card">
+          <img src="" alt="I'm a park image" className="card__image" />
+          <div className="card__heading">
+            <h3 className="card__title">Title</h3>
+            <p className="card__date">date</p>
+          </div>
+          <p className="card__location">location</p>
+          <div className="card__info">
+            <p className="card__info-paragraph">information about park</p>
+            <p className="card__info-highlights">highlights</p>
+          </div>
+        </li>
+
+        <li className="card">
+          <img src="" alt="I'm a park image" className="card__image" />
+          <div className="card__heading">
+            <h3 className="card__title">Title</h3>
+            <p className="card__date">date</p>
+          </div>
+          <p className="card__location">location</p>
+          <div className="card__info">
+            <p className="card__info-paragraph">information about park</p>
+            <p className="card__info-highlights">highlights</p>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Cards;
