0da1d353977fc150372b1d2a63a64a566eb285a0
Matthew Richards <matthewrichards234@gmail.com>
2025-07-20 15:41:30 -0400

    Update card info

--- src/components/ParkCards/ParkCards.jsx
+++ src/components/ParkCards/ParkCards.jsx
@@ -7,13 +7,15 @@ function Cards() {
         <li className="card">
           <img
             src="src/assets/Mountain-clouds.jpg"
-            alt="Great Smoky Mountains"
+            alt="Golden Gate National Recreation Area"
             className="card__image"
           />
 
           <div className="card__content">
             <div className="card__heading">
-              <h3 className="card__title">Great Smoky Mountains</h3>
+              <h3 className="card__title">
+                Golden Gate National Recreation Area
+              </h3>
               <span className="card__badge">
                 🗓️ April - May, September - October
               </span>
@@ -37,29 +39,32 @@ function Cards() {
         <li className="card">
           <img
             src="src/assets/Mountain-clouds.jpg"
-            alt="Great Smoky Mountains"
+            alt="Charles Young Buffalo Soldiers National Monument"
             className="card__image"
           />
 
           <div className="card__content">
             <div className="card__heading">
-              <h3 className="card__title">Great Smoky Mountains</h3>
+              <h3 className="card__title">
+                Charles Young Buffalo Soldiers National Monument
+              </h3>
               <span className="card__badge">
                 🗓️ April - May, September - October
               </span>
             </div>
 
-            <p className="card__location">📍 Tennessee, North Carolina</p>
+            <p className="card__location">📍 Wilberforce, Ohio</p>
 
             <p className="card__description">
-              America's most visited national park, famous for its misty
-              mountains, diverse wildlife, and Appalachian culture.
+              Charles Young Buffalo Soldiers National Monument honors the life
+              and legacy of Colonel Charles Young, a pioneering African American
+              Army officer and leader of the Buffalo Soldiers,
             </p>
 
             <div className="card__highlights">
-              <span className="highlight">Cataract Falls</span>
-              <span className="highlight">Cades Cove</span>
-              <span className="highlight">Appalachian Trail</span>
+              <span className="highlight">Youngsholm</span>
+              <span className="highlight">Buffalo Soldiers Legacy</span>
+              <span className="highlight">Guided Tours & Ranger Programs</span>
             </div>
           </div>
         </li>
@@ -67,29 +72,35 @@ function Cards() {
         <li className="card">
           <img
             src="src/assets/Mountain-clouds.jpg"
-            alt="Great Smoky Mountains"
+            alt="Theodore Roosevelt National Park"
             className="card__image"
           />
 
           <div className="card__content">
             <div className="card__heading">
-              <h3 className="card__title">Great Smoky Mountains</h3>
+              <h3 className="card__title">Theodore Roosevelt National Park</h3>
               <span className="card__badge">
                 🗓️ April - May, September - October
               </span>
             </div>
 
-            <p className="card__location">📍 Tennessee, North Carolina</p>
+            <p className="card__location">📍 Medora, North Dakota</p>
 
             <p className="card__description">
-              America's most visited national park, famous for its misty
-              mountains, diverse wildlife, and Appalachian culture.
+              Theodore Roosevelt National Park preserves the rugged Badlands of
+              western North Dakota—where Theodore Roosevelt found solace,
+              inspiration, and his conservation calling—in three distinct units
+              near Medora
             </p>
 
             <div className="card__highlights">
-              <span className="highlight">Cataract Falls</span>
-              <span className="highlight">Cades Cove</span>
-              <span className="highlight">Appalachian Trail</span>
+              <span className="highlight">
+                Maltese Cross Cabin & Elkhorn Ranch Site
+              </span>
+              <span className="highlight">
+                Spectacular Badlands & Scenic Drives
+              </span>
+              <span className="highlight">Wildlife & Geologic Wonders</span>
             </div>
           </div>
         </li>
@@ -97,29 +108,37 @@ function Cards() {
         <li className="card">
           <img
             src="src/assets/Mountain-clouds.jpg"
-            alt="Great Smoky Mountains"
+            alt="Fort Union National Monument"
             className="card__image"
           />
 
           <div className="card__content">
             <div className="card__heading">
-              <h3 className="card__title">Great Smoky Mountains</h3>
+              <h3 className="card__title">Fort Union National Monument</h3>
               <span className="card__badge">
                 🗓️ April - May, September - October
               </span>
             </div>
 
-            <p className="card__location">📍 Tennessee, North Carolina</p>
+            <p className="card__location">📍Watrous, New Mexico</p>
 
             <p className="card__description">
-              America's most visited national park, famous for its misty
-              mountains, diverse wildlife, and Appalachian culture.
+              Fort Union National Monument preserves the ruins of three U.S.
+              Army forts built from 1851 to 1891 near the junction of the Santa
+              Fe Trail, showcasing the largest 19th-century military fort in the
+              Southwest, now set amid sweeping prairie landscape.
             </p>
 
             <div className="card__highlights">
-              <span className="highlight">Cataract Falls</span>
-              <span className="highlight">Cades Cove</span>
-              <span className="highlight">Appalachian Trail</span>
+              <span className="highlight">
+                Three Successive Fort Sites & Santa Fe Trail Ruts
+              </span>
+              <span className="highlight">
+                Major Civil‑War‑Era Supply Depot
+              </span>
+              <span className="highlight">
+                Guided & Self‑Guided Trails with Ranger Programs
+              </span>
             </div>
           </div>
         </li>
@@ -127,29 +146,37 @@ function Cards() {
         <li className="card">
           <img
             src="src/assets/Mountain-clouds.jpg"
-            alt="Great Smoky Mountains"
+            alt="Fire Island National Seashore"
             className="card__image"
           />
 
           <div className="card__content">
             <div className="card__heading">
-              <h3 className="card__title">Great Smoky Mountains</h3>
+              <h3 className="card__title">Fire Island National Seashore</h3>
               <span className="card__badge">
                 🗓️ April - May, September - October
               </span>
             </div>
 
-            <p className="card__location">📍 Tennessee, North Carolina</p>
+            <p className="card__location">
+              📍 Patchogue / Fire Island, New York
+            </p>
 
             <p className="card__description">
-              America's most visited national park, famous for its misty
-              mountains, diverse wildlife, and Appalachian culture.
+              Fire Island National Seashore protects a 26-mile stretch of
+              pristine barrier island off southern Long Island, preserving
+              dynamic beaches, maritime forests, historic communities, and
+              cultural landmarks near Patchogue and Babylon, New York.
             </p>
 
             <div className="card__highlights">
-              <span className="highlight">Cataract Falls</span>
-              <span className="highlight">Cades Cove</span>
-              <span className="highlight">Appalachian Trail</span>
+              <span className="highlight">Fire Island Lighthouse</span>
+              <span className="highlight">
+                Sunken Forest & Otis Pike Wilderness
+              </span>
+              <span className="highlight">
+                Secluded Beaches & Vibrant Communities
+              </span>
             </div>
           </div>
         </li>
@@ -157,119 +184,39 @@ function Cards() {
         <li className="card">
           <img
             src="src/assets/Mountain-clouds.jpg"
-            alt="Great Smoky Mountains"
+            alt="Mount Rushmore National Memorial"
             className="card__image"
           />
 
           <div className="card__content">
             <div className="card__heading">
-              <h3 className="card__title">Great Smoky Mountains</h3>
+              <h3 className="card__title">Mount Rushmore National Memorial</h3>
               <span className="card__badge">
                 🗓️ April - May, September - October
               </span>
             </div>
 
-            <p className="card__location">📍 Tennessee, North Carolina</p>
+            <p className="card__location">📍 Keystone, South Dakota</p>
 
             <p className="card__description">
-              America's most visited national park, famous for its misty
-              mountains, diverse wildlife, and Appalachian culture.
+              Mount Rushmore National Memorial is a monumental granite sculpture
+              featuring the 60‑foot carved heads of Presidents George
+              Washington, Thomas Jefferson, Theodore Roosevelt, and Abraham
+              Lincoln—symbolizing the birth, growth, development, and
+              preservation of the United States—set high in the forested Black
+              Hills near Keystone.
             </p>
 
             <div className="card__highlights">
-              <span className="highlight">Cataract Falls</span>
-              <span className="highlight">Cades Cove</span>
-              <span className="highlight">Appalachian Trail</span>
-            </div>
-          </div>
-        </li>
-
-        <li className="card">
-          <img
-            src="src/assets/Mountain-clouds.jpg"
-            alt="Great Smoky Mountains"
-            className="card__image"
-          />
-
-          <div className="card__content">
-            <div className="card__heading">
-              <h3 className="card__title">Great Smoky Mountains</h3>
-              <span className="card__badge">
-                🗓️ April - May, September - October
+              <span className="highlight">
+                Avenue of Flags & Grand View Terrace
               </span>
-            </div>
-
-            <p className="card__location">📍 Tennessee, North Carolina</p>
-
-            <p className="card__description">
-              America's most visited national park, famous for its misty
-              mountains, diverse wildlife, and Appalachian culture.
-            </p>
-
-            <div className="card__highlights">
-              <span className="highlight">Cataract Falls</span>
-              <span className="highlight">Cades Cove</span>
-              <span className="highlight">Appalachian Trail</span>
-            </div>
-          </div>
-        </li>
-
-        <li className="card">
-          <img
-            src="src/assets/Mountain-clouds.jpg"
-            alt="Great Smoky Mountains"
-            className="card__image"
-          />
-
-          <div className="card__content">
-            <div className="card__heading">
-              <h3 className="card__title">Great Smoky Mountains</h3>
-              <span className="card__badge">
-                🗓️ April - May, September - October
+              <span className="highlight">
+                Presidential Trail & Sculptor’s Studio
               </span>
-            </div>
-
-            <p className="card__location">📍 Tennessee, North Carolina</p>
-
-            <p className="card__description">
-              America's most visited national park, famous for its misty
-              mountains, diverse wildlife, and Appalachian culture.
-            </p>
-
-            <div className="card__highlights">
-              <span className="highlight">Cataract Falls</span>
-              <span className="highlight">Cades Cove</span>
-              <span className="highlight">Appalachian Trail</span>
-            </div>
-          </div>
-        </li>
-
-        <li className="card">
-          <img
-            src="src/assets/Mountain-clouds.jpg"
-            alt="Great Smoky Mountains"
-            className="card__image"
-          />
-
-          <div className="card__content">
-            <div className="card__heading">
-              <h3 className="card__title">Great Smoky Mountains</h3>
-              <span className="card__badge">
-                🗓️ April - May, September - October
+              <span className="highlight">
+                Evening Lighting Ceremony & Ranger Talks
               </span>
-            </div>
-
-            <p className="card__location">📍 Tennessee, North Carolina</p>
-
-            <p className="card__description">
-              America's most visited national park, famous for its misty
-              mountains, diverse wildlife, and Appalachian culture.
-            </p>
-
-            <div className="card__highlights">
-              <span className="highlight">Cataract Falls</span>
-              <span className="highlight">Cades Cove</span>
-              <span className="highlight">Appalachian Trail</span>
             </div>
           </div>
         </li>
