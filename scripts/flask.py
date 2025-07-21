ae52d9fcb8fd162e41be3816e4357621f5f791b7
paul-london <palondon@hotmail.com>
2025-07-20 22:04:09 -0400

    Update

--- scripts/flask.py
+++ scripts/flask.py
@@ -3,7 +3,7 @@ from map import plot_route_on_map  # import your function
 
 app = Flask(__name__)
 
-@app.route('/api/map', methods=['POST'])
+@app.route('../', methods=['POST'])
 def create_map():
     data = request.get_json()
     home_state = data.get("state")
@@ -11,12 +11,8 @@ def create_map():
     # Call your existing function
     plot_route_on_map(home_state)
 
-    # Option 1: Return file path to React (for iframe)
-    return jsonify({"url": f"/maps/map.html"})
-
-    # Option 2: Return full HTML string
-    # with open("map.html", "r") as f:
-    #     return f.read()
+    # Return file path to React (for iframe)
+    return jsonify({"url": f"../usa_roadtrip_map.html"})
 
 if __name__ == '__main__':
     app.run(debug=True)
