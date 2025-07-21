c2b6fae14cbb4e9a3daaac925bb319a1708af9c6
paul-london <palondon@hotmail.com>
2025-07-21 00:06:29 -0400

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,17 @@
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
+
 function App() {
-  return <h1>Hello, React is working!</h1>;
+  return (
+    <div className="page">
+      <div className="page__content">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
