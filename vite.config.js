34cb003584dbef630b2f9479ef4a1240e8818ad7
paul-london <palondon@hotmail.com>
2025-07-20 23:57:46 -0400

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',            // <---- Add this line
+  base: "./",  // ensures relative paths in dist/
   plugins: [react()],
   server: {
     port: 3000,
