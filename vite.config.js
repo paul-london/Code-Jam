0ba6782351b5e088ee11db3226f4c9028431665e
paul-london <palondon@hotmail.com>
2025-07-22 13:23:54 -0400

    Updates

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/Park-Hopper-Routes/",  // ← required for GitHub Pages
   plugins: [react()],
+  build: {
+    outDir: "docs",  // tells Vite to build into the docs/ folder
+  },
   server: {
     port: 3000,
   },
