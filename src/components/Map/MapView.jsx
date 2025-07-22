fffcb0c582b960869c40cd9fe8b83b5c5ed55909
paul-london <palondon@hotmail.com>
2025-07-22 17:58:26 -0400

    Deploy

--- src/components/Map/MapView.jsx
+++ src/components/Map/MapView.jsx
@@ -97,7 +97,7 @@ function MapView({ selectedState }) {
       <div className="map">
         <iframe
           title="Folium Map"
-          src={`${import.meta.env.BASE_URL}maps/${mapFile}`}
+          src={`/Park-Hopper-Routes/${mapFile}`}
           style={{ width: "100%", height: "100%", border: "none" }}
           onLoad={() => setLoading(false)}
         />
